300e7111980a191e5ff5e6a6e5a0b1a33008e12d
Cheng Lian <lian@databricks.com>
2015-05-11 20:41:09 +0800

    Minor bug fix and more tests

--- sql/core/src/main/scala/org/apache/spark/sql/sources/commands.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/sources/commands.scala
@@ -337,7 +337,7 @@ private[sql] abstract class BaseWriterContainer(
   }
 
   def abortJob(): Unit = {
-    // outputCommitter.abortJob(jobContext, JobStatus.State.FAILED)
+    outputCommitter.abortJob(jobContext, JobStatus.State.FAILED)
     logError(s"Job $jobId aborted.")
   }
 }
